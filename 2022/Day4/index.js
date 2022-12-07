b29181d54d7f2d27bb1db539adab4d821a660ae2
d.welborn <d.welborn@samsung.com>
2022-12-07 16:39:32 -0500

    Completed Day 4

--- 2022/Day4/index.js
+++ 2022/Day4/index.js
@@ -1,5 +1,67 @@
 /*
---- Day X: Description ---
+--- Day 4: Camp Cleanup ---
+
+Space needs to be cleared before the last supplies can be unloaded from the ships, and so several Elves have been assigned the job of cleaning up sections of the camp. Every section has a unique ID number, and each Elf is assigned a range of section IDs.
+
+However, as some of the Elves compare their section assignments with each other, they've noticed that many of the assignments overlap. To try to quickly find overlaps and reduce duplicated effort, the Elves pair up and make a big list of the section assignments for each pair (your puzzle input).
+
+For example, consider the following list of section assignment pairs:
+
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+
+For the first few pairs, this list means:
+
+    Within the first pair of Elves, the first Elf was assigned sections 2-4 (sections 2, 3, and 4), while the second Elf was assigned sections 6-8 (sections 6, 7, 8).
+    The Elves in the second pair were each assigned two sections.
+    The Elves in the third pair were each assigned three sections: one got sections 5, 6, and 7, while the other also got 7, plus 8 and 9.
+
+This example list uses single-digit section IDs to make it easier to draw; your actual list might contain larger numbers. Visually, these pairs of section assignments look like this:
+
+.234.....  2-4
+.....678.  6-8
+
+.23......  2-3
+...45....  4-5
+
+....567..  5-7
+......789  7-9
+
+.2345678.  2-8
+..34567..  3-7
+
+.....6...  6-6
+...456...  4-6
+
+.23456...  2-6
+...45678.  4-8
+
+Some of the pairs have noticed that one of their assignments fully contains the other. For example, 2-8 fully contains 3-7, and 6-6 is fully contained by 4-6. In pairs where one assignment fully contains the other, one Elf in the pair would be exclusively cleaning sections their partner will already be cleaning, so these seem like the most in need of reconsideration. In this example, there are 2 such pairs.
+
+In how many assignment pairs does one range fully contain the other?
+
+Your puzzle answer was 513.
+
+The first half of this puzzle is complete! It provides one gold star: *
+--- Part Two ---
+
+It seems like there is still quite a bit of duplicate work planned. Instead, the Elves would like to know the number of pairs that overlap at all.
+
+In the above example, the first two pairs (2-4,6-8 and 2-3,4-5) don't overlap, while the remaining four pairs (5-7,7-9, 2-8,3-7, 6-6,4-6, and 2-6,4-8) do overlap:
+
+    5-7,7-9 overlaps in a single section, 7.
+    2-8,3-7 overlaps all of the sections 3 through 7.
+    6-6,4-6 overlaps in a single section, 6.
+    2-6,4-8 overlaps in sections 4, 5, and 6.
+
+So, in this example, the number of overlapping assignment pairs is 4.
+
+In how many assignment pairs do the ranges overlap?
+
 
 
 */
@@ -11,25 +73,29 @@ const DEBUG = true;
 
 async function setup() {
     const config = {
-        year: null,
-        day: null
+        year: 2022,
+        day: 4
         // ignoreFile: true,
     };
     let rawInput = await Utilities.getInput(config);
-    console.log('rawInput :>> ', rawInput);
-
-    return;
+    // console.log('rawInput :>> ', rawInput);
+    // return;
 
     const parseConfig = {
         rawInput,
-        ignoreFile: true,
+        // ignoreFile: true,
         parseFunction: (text) => {
             text = text.trim();
             let output = text.split('\n');
-            output = output.map((x) => {
-                return x;
+            output = output.map((pairString) => {
+                let eachPair = pairString.split(',');
+                let range1Array = eachPair[0].split('-');
+                let range2Array = eachPair[1].split('-');
+                let range1 = { start: Number(range1Array[0]), end: Number(range1Array[1]) };
+                let range2 = { start: Number(range2Array[0]), end: Number(range2Array[1]) };
+                return { range1, range2 };
             });
-            console.log('output :>> ', output);
+            // console.log('output :>> ', output);
             return output;
         }
     };
@@ -39,23 +105,46 @@ async function setup() {
     main(input);
 }
 
+Object.defineProperty(Object.prototype, 'contains', {
+    enumerable: false,
+    value: function (obj) {
+        return obj.start >= this.start && obj.end <= this.end;
+    }
+});
+
+Object.defineProperty(Object.prototype, 'overlaps', {
+    enumerable: false,
+    value: function (obj) {
+        if (this.start <= obj.start && obj.start <= this.end) return true;
+        if (this.start <= obj.end && obj.end <= this.end) return true;
+        return false;
+    }
+});
+
 function main(input) {
-    // console.log("input", input);
+    console.log('input', input);
     let startTime = Utilities.getNanoSecTime();
     let part1 = null;
     let part2 = null;
 
-    input.forEach((entry) => {
-        // if (DEBUG) console.log('=========================================');
-        // if (DEBUG) console.log('=========================================');
+    let part1ContainsArray = input.filter(({ range1, range2 }) => {
+        return range1.contains(range2) || range2.contains(range1);
     });
+
+    part1 = part1ContainsArray.length;
+
+    let part2OverlapsArray = input.filter(({ range1, range2 }) => {
+        return range1.overlaps(range2) || range2.overlaps(range1);
+    });
+
+    part2 = part2OverlapsArray.length;
 
     let endTime = Utilities.getNanoSecTime();
     let timeElapsed = (endTime - startTime) * 0.000001;
     console.log('timeElapsed:', timeElapsed);
 
-    console.log('Part 1:', part1);
-    console.log('Part 2:', part2);
+    console.log('Part 1:', part1); //513
+    console.log('Part 2:', part2); //878
 }
 
 setup();
